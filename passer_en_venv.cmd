48e0968f0a2d9e4d1c279ea5dfba85d103387b89
nicotrorigolo <88196777+nicotrorigolo@users.noreply.github.com>
2022-07-09 04:00:47 +0200

    Add files via upload

---
+++ passer_en_venv.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+echo          ________________________ 
+echo         /                        \
+echo          Coucou, c'est parti ! :D 
+echo         \_   ____________________/
+echo           \ /  
+echo            V
+echo        (\__/)
+echo        (='.'=)
+echo        (")_(")
+
+doskey venv=C:\Users\ms189\Desktop\robots\venv\Scripts\activate.bat
