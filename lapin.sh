c4b885c47dcd97c644fa639f958257ea59cba418
nicotrorigolo <88196777+nicotrorigolo@users.noreply.github.com>
2022-10-04 23:46:23 +0200

    Add files via upload

---
+++ lapin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo          "         ________________________"
+echo         '        /                        \'
+echo          "         Coucou, c'est parti ! :D"
+echo         "        \_   ____________________/"
+echo           "          \ /"
+echo            "           V"
+echo        "        (\__/)"
+echo        "        (='.'=)"
+echo        '        (")_(")'
+echo
+
+
+
